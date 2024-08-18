d769d421933f37db30cd0ba0a187d4f683ce6787
Anatoli Arlouski <aarlouski@refinedinnovators.com>
2024-08-18 15:47:55 -0500

    added Google Oauth 2.0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'; // Import your main App component
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const container = document.getElementById('root');
+const root = createRoot(container); // Create a root for rendering
 root.render(
-  <React.StrictMode>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
     <App />
-  </React.StrictMode>
+  </GoogleOAuthProvider>
 );
-
